eb7ad352564f957c61d3dd1947b075bbec4f4ed5
Ashley Blewer <ashley.blewer@gmail.com>
2023-05-02 12:28:39 -0400

    change social color

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -9,7 +9,7 @@ module.exports = {
   author: 'Ashley Blewer', // i.e. Lene Saile - author's name. Must be set.
   authorEmail: 'ashley.blewer@gmail.com', // i.e. hola@lenesaile.com - email of the author
   authorWebsite: 'https://ashleyblewer.com', // i.e. https.://www.lenesaile.com - the personal site of the author
-  themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
+  themeColor: '#008ed6', //  Manifest: defines the default theme color for the application
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
